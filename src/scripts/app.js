7825ea1fe0bebf5f8a67b638042efc4c92aa984f
sergiomunozgesell <sergioismaelmunozgesell@gmail.com>
2026-03-28 00:39:42 -0300

    Se añaden estilos a login, más funcionalidades básicas de la misma page, pendiente añadir parrafo de configuración en el pie del username

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,8 +6,32 @@
 // LÓGICA
 document.addEventListener('DOMContentLoaded', ()=>{
 
+    const loginPage = document.getElementById("loginPage")
     const loginContainer = document.getElementById("login-users-content");
-    
+    const loginAcces = document.getElementById('login-acces');
+
+    loginAcces.addEventListener('mouseover', (e)=>{
+        loginContainer.classList.add("focus");
+    });
+    loginAcces.addEventListener('mouseleave',()=>{
+        loginContainer.classList.remove("focus");
+    } )
+
+    loginAcces.addEventListener('click', async ()=>{
+
+        loginContainer.classList.remove("focus");
+        loginContainer.classList.add("clicked");
+        // Click y en un periodo de 4 segundos debe cerrar pestaña
+        // Durante ese perido va reproducir el soundtrack de login de windows
+        // Se añadirá estilos de clicked, que mantendrá el bockground gradiente y además se añadira un elemento p de setting configuration
+        // Pasado los 4 segundos se añade el display none para quitar el elemento de la pantalla
+        
+        setInterval(()=>{
+            loginPage.style.display="none"
+        },4000)
+        
+        
+    })
 
 
 
